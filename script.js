fba98ca7ea30f959f681752fe4bdb0e228d66eed
Abhishek Mishra <cs.abhishekmo@gmail.com>
2023-05-22 22:11:46 +0530

    added loading img

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 async function nextMeme() {
+    const memeImg = document.getElementById("meme-img");
+    const nextMemeBtn = document.getElementById("next-meme-btn");
+    memeImg.src = "./loading-splash.gif"
+
     const url = "https://meme-api.com/gimme";
     let req = await fetch(url);
     let resp = await req.json();
-    document.getElementById("meme-img").src = resp["url"];
+    memeImg.src = resp["url"];
 }
 
 nextMeme();
