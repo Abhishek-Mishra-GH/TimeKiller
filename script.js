44c6521eaef83336b14e94b8075c9b097d97e87f
Abhishek Mishra <cs.abhishekmo@gmail.com>
2023-05-24 22:38:29 +0530

    added costum subreddit bar

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-async function nextMeme() {
-    const memeImg = document.getElementById("meme-img");
-    const nextMemeBtn = document.getElementById("next-meme-btn");
-    memeImg.src = "./loading-splash.gif"
+let subreddit = "/";
+const memeImg = document.getElementById("meme-img");
+const nextMemeBtn = document.getElementById("next-meme-btn");
+const setBtn = document.getElementById("set-btn");
 
-    const url = "https://meme-api.com/gimme";
+async function nextMeme() {
+    memeImg.src = "./loading-splash.gif";
+
+    let url = "https://meme-api.com/gimme";
+    if (subreddit != "/") url += subreddit;
+
     let req = await fetch(url);
     let resp = await req.json();
     memeImg.src = resp["url"];
+    nextMemeBtn.style.color = "white";
+    setBtn.style.color = "white";
+}
+
+function set() {
+    subreddit = '/';
+    subreddit += document.getElementById("subreddit").textContent.trim();
+    nextMeme();
+    setBtn.style.color = "white";
 }
 
 nextMeme();
